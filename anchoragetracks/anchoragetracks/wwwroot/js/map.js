c335f838ca9058bb34a5402af70a3885f854fca2
Samuel Grosenick <srgrosenick@gmail.com>
2020-06-28 11:02:40 -0800

    added map

---
+++ anchoragetracks/anchoragetracks/wwwroot/js/map.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    var map = L.map('map').setView([61.217381, -149.863129], 10);
+
+    //L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+    //    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //}).addTo(map);
+
+    var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    });
+
+    Stadia_AlidadeSmoothDark.addTo(map);
+
+})();
